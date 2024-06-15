3dab9eff6a7347082b28fc28760077de3ea538ed
Naruto92devs <m.suhaibmpdgw@gmail.com>
2024-06-15 11:25:18 +0500

    adding nav component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
+import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
-    <h1>
-      Home Page
-    </h1>
+    <div className="home">
+      <Navbar/>
+      <h1 className="text-sm hover:text-7xl">
+        Home Page
+      </h1>
+    </div>
   );
 }
 
