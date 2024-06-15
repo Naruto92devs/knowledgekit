b474e3bd87e9c1436de524ca38cb400141399d51
Naruto_Webdevs <114051176+Naruto92devs@users.noreply.github.com>
2024-06-15 11:27:44 +0500

    Dev naruto92 (#19)
    * resolve the problem of getting the network url to visit the app
    
    * adding rules
    
    * improving next dev
    
    * edited ip
    
    * adding nav component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
+import Navbar from '../components/Navbar';
 
 import Footer from "@/components/footer";
 const Home = () => {
   return (
     <div className="home">
-      <h1>Home</h1>
+      <Navbar/>
+      <h1 className="text-sm hover:text-7xl">
+        Home Page
+      </h1>
       <Footer/>
-
     </div>
   );
 }
