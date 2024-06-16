2f02424d6b21ca6ff5491272c3c92cedb0fddda7
MOHAMMAD AMIR <139647868+Malik4686@users.noreply.github.com>
2024-06-16 09:25:43 +0500

    Dev amir (#20)
    * new commit about footer
    
    * worked on footer

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Footer from "@/components/footer";
 const Home = () => {
   return (
     <div className="home">
+
       <Navbar/>
       <h1 className="text-sm hover:text-7xl">
         Home Page
